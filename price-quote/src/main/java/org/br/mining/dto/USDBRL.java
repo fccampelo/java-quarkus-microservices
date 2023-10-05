bd7cdc5faef7ef8fa2535f41eb7f8287773cb443
Felipe Campelo <fcampelocc@gmail.com>
2023-10-05 09:31:47 -0300

    Creating CurrencyPriceDTO QoutationDTO USDBRL dto's

---
+++ price-quote/src/main/java/org/br/mining/dto/USDBRL.java
@@ -0,0 +1,25 @@
+package org.br.mining.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
+
+@Jacksonized
+@Data
+@Builder
+@AllArgsConstructor
+public class USDBRL {
+
+    public String code;
+    public String codein;
+    public String name;
+    public String high;
+    public String low;
+    public String varBid;
+    public String pctChange;
+    public String bit;
+    public String ask;
+    public String timestamp;
+    public String created_date;
+}
